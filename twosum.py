39657e780423f7bc5ca190ab84a7f2b1d9ede0c3
Erin0910 <yangxueying0910@hotmail.com>
2017-02-20 13:15:26 +0100

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -12,15 +12,13 @@ def twoSum(nums, target):
 
 twoSum([3,2,4],6)
 
-"""
-class Solution(object):
-    def twoSum(self, nums, target):
-        if len(nums) <= 1:
-            return False
-        buff_dict = {}
-        for i in range(len(nums)):
-            if nums[i] in buff_dict:
-                return [buff_dict[nums[i]], i]
-            else:
-                buff_dict[target - nums[i]] = i
- """
+# class solution(object):
+#     def twosum(self, nums, target):
+#         if len(nums) <= 1:
+#             return false
+#         buff_dict = {}
+#         for i in range(len(nums)):
+#             if nums[i] in buff_dict:
+#                 return [buff_dict[nums[i]], i]
+#             else:
+#                 buff_dict[target - nums[i]] = i
